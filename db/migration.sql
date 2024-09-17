80c0adeb9846415a7d3647a0d1e2fd97f7887f3b
Default Programmer <defproger@gmail.com>
2024-09-17 11:11:39 +0300

    fix

--- db/migration.sql
+++ db/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE fibonacci_requests
     id               INT AUTO_INCREMENT PRIMARY KEY,
     username         VARCHAR(255) NOT NULL,
     user_input       INT          NOT NULL,
-    fibonacci_number INT          NOT NULL,
+    fibonacci_number text         NOT NULL,
     ip_address       VARCHAR(255) NOT NULL,
     created_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
